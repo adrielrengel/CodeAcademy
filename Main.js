571887373fd70d760ecc14846bd530f9efa90450
adrielrengel <adrielrengel@gmail.com>
2015-11-24 10:21:15 -0200

    Create Main.js
    CodeAcademy test

---
+++ Main.js
@@ -0,0 +1,20 @@
+    var red = [0, 100, 63];
+        var orange = [40, 100, 60];
+        var green = [75, 100, 40];
+        var black = [196, 77, 55];
+        var purple = [280, 50, 60];
+
+        var myName = "Rorschach";   
+        letterColors = [red, orange, green, purple, black]
+        if(10 < 3) {
+            bubbleShape = "square";
+        }
+
+        else {
+            bubbleShape = "circle";
+        }
+
+
+        drawName(myName, letterColors);
+        
+        bounceBubbles();
